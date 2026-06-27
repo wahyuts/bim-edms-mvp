6b35e4fe63ab07eba5ea574e49595fc0128db851
WahyuTs <wahyu.trisna100@gmail.com>
2026-06-27 10:50:02 +0700

    update cons lagi

--- js/utils/constants.js
+++ js/utils/constants.js
@@ -1,6 +1,6 @@
 export const APP_NAME = "SENA EDMS";
-export const API_BASE_URL = window.EDMS_API_BASE_URL || "http://localhost:4000/api/v1";
-// export const API_BASE_URL = window.EDMS_API_BASE_URL || "https://bim-edms-mvp-production.up.railway.app/api/v1";
+// export const API_BASE_URL = window.EDMS_API_BASE_URL || "http://localhost:4000/api/v1";
+export const API_BASE_URL = window.EDMS_API_BASE_URL || "https://bim-edms-mvp-production.up.railway.app/api/v1";
 
 export const ROUTES = {
   login: "/login",
